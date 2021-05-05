baa329f6a91dce79a527ee538707e94c327f50fe
DeliciousBoy <83308832+DeliciousBoy@users.noreply.github.com>
2021-05-06 03:06:50 +0700

    Update Exercise4_Wittaya_T.py

--- Exercise4_Wittaya_T.py
+++ Exercise4_Wittaya_T.py
@@ -3,7 +3,7 @@ FDT_Eng = "47.25"
 GN_bus = "68.00"
 ITCS = "93.00"
 CP = "82.03"
-#รวมคะแนนทั้งหมดที่ได้
+#รวมคะแนนทั้งหมดที่ได้และแสดงผลลัพธ์ออกมา
 score = print("---Your Score---","\n"
               +"Foundation English:"+FDT_Eng,"\n"
               +"General Business:"+GN_bus,"\n"
