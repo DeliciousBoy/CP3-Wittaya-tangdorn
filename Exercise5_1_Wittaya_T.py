c8d69ddbc1eac01257ea52ba420fdc9f23371c6a
DeliciousBoy <outdata.gide@gmail.com>
2021-05-07 20:28:57 +0700

    add exercise5_2

---
+++ Exercise5_1_Wittaya_T.py
@@ -0,0 +1,10 @@
+a = int(input("first number: "))
+b = int(input("second number: "))
+print(a,"+",b,"=",a+b,"\n"
+    ,a,"-",b,"=",a-b,"\n"
+    ,a,"*",b,"=",a*b,"\n"
+    ,a,"/",b,"=",a/b)
+
+
+
+
---
+++ Exersice5_2_Wittaya_T.py
@@ -0,0 +1,4 @@
+distance = float(input("ระยะทางที่เคลื่อนทีได้(km):"))
+time = float(input("ระยะเวลาจากจุดเริ่มต้นถึงจุดสิ้นสุด(h):"))
+velocity = distance/time
+print(velocity,"km/h")
