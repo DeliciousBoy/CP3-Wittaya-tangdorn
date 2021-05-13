e32572dab9de92ff00e942eba74062a68aa22d96
DeliciousBoy <outdata.gide@gmail.com>
2021-05-14 01:32:44 +0700

    Create Lecture50_Wittaya_T.py

---
+++ Lecture50_Wittaya_T.py
@@ -0,0 +1,7 @@
+def mathematical(x,y):
+    print("","--Mathematical--")
+    print("",x,"+",y,"=",x+y,"\n"
+    ,x,"-",y,"=",x-y,"\n"
+    ,x,"x",y,"=",x*y,"\n"
+    ,x,"/",y,"=",x/y)
+mathematical(5,2)
