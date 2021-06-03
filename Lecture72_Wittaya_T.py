8bc08c732eafb9c4585062a2abc1d609b0f2d5cd
DeliciousBoy <outdata.gide@gmail.com>
2021-06-03 16:43:14 +0700

    Update Lecture72_Wittaya_T.py

--- Lecture72_Wittaya_T.py
+++ Lecture72_Wittaya_T.py
@@ -2,16 +2,20 @@ menulist = []
 
 def bill():
     print("Food".center(20,"-"))
+    total = 0
     for number in range(len(menulist)):
         print(menulist[number][0], menulist[number][1])
+        total += int(menulist[number][1])
+    print(total)
 
 while True:
     menuname = input("please enter Menu:")
     if menuname.lower() == "exit":
         break
     else:
-        menuprice = input("menu price: ")
+        menuprice = int(input("menu price: "))
         menulist.append([menuname, menuprice])
+
 
 bill()
 
